05fa9e07a1840e7d524c85c0f2f402e0e81b4c73
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-23 23:04:34 +0530

    ADDED: Handle NullPointerException and return Happy

--- src/main/java/com/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -12,8 +12,12 @@ public class MoodAnalyser {
     }
 
     public String analyse() {
-        if (message.contains("Sad"))
-            return "SAD";
-        return "HAPPY";
+        try {
+            if (message.contains("Sad"))
+                return "SAD";
+            return "HAPPY";
+        }catch (NullPointerException e){
+            return "HAPPY";
+        }
     }
 }
